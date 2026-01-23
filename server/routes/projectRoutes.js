bd7e3531fd605d70d81adc54fef063a8aaf758da
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-01-23 16:16:26 +0100

    starting projectRoutes

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -2,4 +2,6 @@ import express from 'express';
 import {} from '../controllers/projectController.js';
 const projectRouter = express.router();
 
-projectRouter.post('/projects', createProject);
+projectRouter.post('/', createProject);
+projectRouter.put('/', updateProject);
+projectRouter.post('/add-member', addMemberToProject);
