3e3cecd2e725bb31c410e1a088d04480aac728de
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-01-23 18:48:28 +0100

    delete task function

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -51,7 +51,6 @@ export const updateTask = async (req, res) => {
     try {
         const { userId } = await req.auth();
         const{ taskId } = req.params;
-         const origin = req.get('origin')
         const task = await prisma.task.findUnique({
             where: { id: taskId }
         });
@@ -82,3 +81,40 @@ export const updateTask = async (req, res) => {
         res.status(500).json({ message: "Internal server error"});
     }
 };
+//Delete task
+export const deleteTask = async (req, res) => {
+    try {
+        const { userId } = await req.auth();
+        const{ tasksIds } = req.body;
+        const tasks = await prisma.task.findMany({
+            where: { id: {in : tasksIds} }
+        });
+        if(tasks.length ===0){
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+           //check if user has admin role for the project 
+        const project = await prisma.project.findUnique({
+         where: { id: tasks[0].projectId },
+         include: { members : {include : {user:true}}}
+        });
+
+        if(!project){
+             return res.status(404).json({ message: "Project not found" });
+        }
+        else if (project.team_lead !== userId){
+              return res.status(403).json({ message: "Forbidden: You do not have permission to create task in this project"});
+        }
+
+       await prisma.task.deleteMany({
+        where: { id: { in: tasksIds } }
+        });
+        
+        return res.status(200).json({ message: "Task deleted successfully"} );
+    }
+    catch(error){
+        console.error("Error creating task:", error);
+        res.status(500).json({ message: "Internal server error"});
+    }
+};
+
